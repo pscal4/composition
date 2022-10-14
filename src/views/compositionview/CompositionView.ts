31cc226ebfc3bdb010920849b4776130ef43a0d4
Patti Scalfano <pjs@foundationsoft.com>
2022-10-14 14:49:36 -0400

    CompositionView

--- src/views/compositionview/CompositionView.ts
+++ src/views/compositionview/CompositionView.ts
@@ -8,21 +8,29 @@ export default defineComponent({
   components: { JobList },
   setup() {
     const jobs = ref<Job[]>([
-        { title: 'farm worker', location: 'lon lon ranch', salary: 30000, id: '1' },
-        { title: 'quarryman', location: 'death mountain', salary: 40000, id: '2' },
-        { title: 'flute player', location: 'the lost woods', salary: 35000, id: '3' },
-        { title: 'fisherman', location: 'lake hylia', salary: 21000, id: '4' },
-        { title: 'prison guard', location: 'gerudo valley', salary: 32000, id: '5' }
-      ])
-  
+      { title: 'farm worker', location: 'lon lon ranch', salary: 30000, id: '1' },
+      { title: 'quarryman', location: 'death mountain', salary: 40000, id: '2' },
+      { title: 'flute player', location: 'the lost woods', salary: 35000, id: '3' },
+      { title: 'fisherman', location: 'lake hylia', salary: 21000, id: '4' },
+      { title: 'prison guard', location: 'gerudo valley', salary: 32000, id: '5' }
+    ])
 
     const order = ref<OrderTerm>('title')
 
     const onHandleSortClick = (term: OrderTerm) => {
       order.value = term
-    }    
-     
-    return { jobs, order, onHandleSortClick }
+    }
+
+    function onResetOrder() {
+      order.value = 'title';
+    }
+
+    return {
+      jobs,
+      order,
+      onHandleSortClick,
+      onResetOrder,
+    }
   }
 });
 
--- src/views/compositionview/CompositionView.vue
+++ src/views/compositionview/CompositionView.vue
@@ -1,19 +1,19 @@
 <template>
-    <div>
-      <header>
-        <div class="title">
-          <h1>Composition</h1>
-        </div>
-        <div class="order">
-          <button @click="onHandleSortClick('title')">Order by title</button>
-          <button @click="onHandleSortClick('salary')">Order by salary</button>
-          <button @click="onHandleSortClick('location')">Order by location</button>
-        </div>
-      </header>
-  
-      <JobList :jobs="jobs" :order="order" :showHeading="true" />
-    </div>
-  </template>
+  <div>
+    <header>
+      <div class="title">
+        <h1>Composition</h1>
+      </div>
+      <div class="order">
+        <button @click="onHandleSortClick('title')">Order by title</button>
+        <button @click="onHandleSortClick('salary')">Order by salary</button>
+        <button @click="onHandleSortClick('location')">Order by location</button>
+      </div>
+    </header>
+
+    <JobList :jobs="jobs" :order="order" :showHeading="true" someNumber="200" @resetOrder="onResetOrder"/>
+  </div>
+</template>
   <script src="./CompositionView.ts" lang="ts"/>
   <style>
   header {
